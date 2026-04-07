608bcc84843e5f9c0b80e078aa31cc7aaae84002
YehorReil <egorrejl44@gmail.com>
2026-04-08 00:46:56 +0300

    feat: add config

---
+++ internal/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+type (
+	RedisHostType     string
+	RedisPortType     string
+	RedisPasswordType string
+
+	HTTPPortType string
+)
+type Config struct {
+	HTTPPort      HTTPPortType      `env:"HTTP_PORT" envDefault:"8080"`
+	RedisHost     RedisHostType     `env:"REDIS_HOST" envDefault:"redis"`
+	RedisPort     RedisPortType     `env:"REDIS_PORT" envDefault:"6379"`
+	RedisPassword RedisPasswordType `env:"REDIS_PASSWORD" envDefault:"redis_password"`
+}
