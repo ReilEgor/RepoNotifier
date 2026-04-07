cba4f20013c8753ac1a81644e93ee222967740d2
YehorReil <egorrejl44@gmail.com>
2026-04-08 01:12:34 +0300

    feat: add dsn in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,8 +6,10 @@ type (
 	RedisPasswordType string
 
 	HTTPPortType string
+	DSNType      string
 )
 type Config struct {
+	DSN           DSNType           `env:"DB_SOURCE" envDefault:"postgres://user:password@localhost:5432/userservice?sslmode=disable"`
 	HTTPPort      HTTPPortType      `env:"HTTP_PORT" envDefault:"8080"`
 	RedisHost     RedisHostType     `env:"REDIS_HOST" envDefault:"redis"`
 	RedisPort     RedisPortType     `env:"REDIS_PORT" envDefault:"6379"`
