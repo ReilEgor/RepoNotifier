68c2a8dd919e04adba84db5413cdf58bd3243789
YehorReil <egorrejl44@gmail.com>
2026-04-12 23:15:07 +0300

    fix: change cache from 1 to 10

--- internal/infrastructure/clients/github/github_client.go
+++ internal/infrastructure/clients/github/github_client.go
@@ -20,7 +20,7 @@ const (
 	httpClientTimeout = 10 * time.Second
 
 	// Cache TTL
-	cacheTTL = 1 * time.Minute
+	cacheTTL = 10 * time.Minute
 
 	// Cache key prefixes
 	cacheKeyRepoExists    = "repo_exists:"
